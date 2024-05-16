6d198279a0da045fc332ebab1cf802d4c3dbb688
Jorge Vilca <jlvilcav@gmail.com>
2024-05-16 01:11:57 -0500

    Creación de tablas

---
+++ database/migrations/2024_05_16_052921_create_personas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id('idPersona');
+            $table->string('apPaterno', 50);
+            $table->string('apMaterno', 50);
+            $table->string('nombres', 50);
+            $table->string('numeroDocumento', 15);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
